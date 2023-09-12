58eef2d26cf4448143a3610b608faad18e75d16e
kbuterba <64853910+kbuterba@users.noreply.github.com>
2023-09-11 20:40:12 -0400

    working authentication

---
+++ google_photos/auth.py
@@ -0,0 +1,35 @@
+import pickle
+import os.path
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+TOKEN_PATH = 'creds/token.pickle'
+
+creds = None
+
+
+def get_credentials():
+    """ Updates Credentials """
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    global creds
+    if creds and creds.valid:
+        return creds
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'creds/credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open(TOKEN_PATH, 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
