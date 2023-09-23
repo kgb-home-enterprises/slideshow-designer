2da826c57a8abf92b407838b71d910aae0471f6a
kbuterba <64853910+kbuterba@users.noreply.github.com>
2023-09-23 10:39:16 -0400

    support to download album

--- src/google_photos/google_photos_api_service.py
+++ src/google_photos/google_photos_api_service.py
@@ -1,7 +1,8 @@
 import json
+import math
+import requests
 
 import src.google_photos.auth as auth
-import requests
 
 
 class GooglePhotosApiService:
@@ -41,7 +42,24 @@ class GooglePhotosApiService:
         payload = {
             'albumId': album_id
         }
-        response = requests.get(self.endpoint + '/mediaItems:search',
-                                data=json.dumps(payload), headers=self.headers)
+        response = requests.post(self.endpoint + '/mediaItems:search', data=json.dumps(payload), headers=self.headers)
         response.raise_for_status()
-        return response
+        return response.json()['mediaItems']
+
+    @staticmethod
+    def download_photo(base_url, file_path):
+        img_data = requests.get(base_url).content
+        with open(file_path, 'wb') as handler:
+            handler.write(img_data)
+
+    def download_album_to_folder(self, album_id, path_to_folder, rename_to_order=False):
+        album_contents = self.get_album_contents(album_id)
+        digits = math.ceil(math.log(len(album_contents), 16))
+        ordered_photos = []
+        for i, photo in enumerate(album_contents):
+            filename = photo['filename']
+            if rename_to_order:
+                filename = '{num:0{width}x}'.format(num=i, width=digits) + '_' + filename
+            self.download_photo(photo['baseUrl'] + '=d', path_to_folder + '/' + filename)
+            ordered_photos.append(filename)
+        return ordered_photos
